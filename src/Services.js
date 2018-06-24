97eeb9dc985ba051c9eab2e48685d50936e01bc4
kasseyrodriguez <kasseyrodriguez@yahoo.com>
2018-06-24 13:19:45 -0400

    init services

---
+++ src/Services.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+
+function sleep(ms){
+  return new Promise(resolve => setTimeout(resolve, ms))
+
+}
+
+class Services extends Component {
+
+
+async fetchData(){
+  let api_key = '32693030a396d661b1acb7b1699c8760';
+
+
+   let data1 = await axios.get("https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=" + api_key).catch(this.awaitErrorHandler);
+   await sleep(3000);
+   let data2 = await axios.get("https://api.betterdoctor.com/2016-03-01/practices?location=37.773,-122.413,10&skip=2&limit=10&user_key=" + api_key).catch(this.awaitErrorHandler);
+   console.log(data1.data);
+   console.log(data2.data);
+}
+ awaitErrorHandler(err) {
+ console.log(err);
+}
+
+   render() {
+
+this.fetchData();
+
+return(
+<p>This works</p>
+)
+
+
+  }
+}
+
+export default Services;
