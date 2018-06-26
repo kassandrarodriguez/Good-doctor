138cbfbe06f167f1b2a0f021b7b0d7fdf606f41c
kasseyrodriguez <kasseyrodriguez@yahoo.com>
2018-06-26 12:56:22 -0400

    modify form

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,24 +2,10 @@ import React from 'react';
 // import 'css';
 
 class Form extends React.Component {
-  // getWeather(event) {
-  //   event.preventDefault();
-  //   const weatherId = this.weatherFormInput.value;
-  //   let path = `/weather/${ weatherId }`;
-  //   this.props.history.push(path);
-  // }
+
   render() {
     return (
-      <form action="" className="form" onSubmit={ (e) => this.getWeather(e) }>
-        <h1 className="form-title">Find your local Doctor!</h1>
-        <div>
-          <label htmlFor="city">Enter Insurance:</label>
-          <input type="text" id="city" name="city" placeholder="ex: 33024" ref={ (input) => this.weatherFormInput = input } />
-        </div>
-        <footer>
-          <input type="submit" value="Find my Doctor!" />
-        </footer>
-      </form>
+  
     )
   }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,8 @@ class Home extends React.Component{
 <div>
   <div class="ui buttons">
     <button class="ui button" role="button"onClick={e => this.props.history.push("/")}>Home</button>
-      <button class="ui button" role="button"onClick={e => this.props.history.push("/")}>About</button>
+      <button class="ui button" role="button"onClick={e => this.props.history.push("/doctors")}>Services</button>
+      <button class="ui button" role="button"onClick={e => this.props.history.push("/about")}>About</button>
 
   </div>
   <body>
@@ -17,7 +18,7 @@ class Home extends React.Component{
           <div className="hero-inner">
             <h1>Get as Good as it Gets!</h1>
             <p>The Easiest Way to Locate <br /> Highly Rated Pediatricians in Miami.</p>
-            <img className="logo" src="Images/GOODr_Logo.png" alt="Pediatrician and Child"/>
+            <img className="logo" src="../Images/GOODr_Logo.png" alt="Pediatrician and Child"/>
           </div>
 
           <div className="bottom">
