da39d194f3a3285eda6542f7a4b044a70ffaf70d
kasseyrodriguez <kasseyrodriguez@yahoo.com>
2018-06-26 16:47:51 -0400

    Make Search Bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Action } from "semantic-ui-react";
+
 
 export default function Search(props) {
   return (
-    <div className="bottom">
-      <form className="search" action="#" method="post">
-        <input type="text" name="insurance" placeholder="Insurance Provider" />
-        <input type="button" name="submit" value="SEARCH" onClick={props.fetchData}/>
-      </form>
-    </div>
-  );
+    <div class="ui action input">
+    <input type="text" placeholder="Provide Insurance Provider..."/>
+    <button class="ui button" name="submit" value="SEARCH" onClick={props.fetchData}>Search</button>
+  </div>
+)
+  //
+  //   <div className="bottom">
+  //     <form className="search" action="#" method="post">
+  //       <input type="text" name="insurance" placeholder="Insurance Provider" />
+  //       <input type="button" name="submit" value="SEARCH" onClick={props.fetchData}/>
+  //     </form>
+  //   </div>
+  // );
 }
