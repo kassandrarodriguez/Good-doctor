0f6380c6e88ebfeaeb00567d68280a06a1aff698
kasseyrodriguez <kasseyrodriguez@yahoo.com>
2018-06-27 16:04:58 -0400

    Changes

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -5,7 +5,10 @@ import React, { Component } from "react";
 export default class Doctor extends Component {
     render(){
       return(
-<p>hello</p>
+
+
+<p></p>
+
       );
     }
 }
--- src/components/DoctorList.js
+++ src/components/DoctorList.js
@@ -6,19 +6,28 @@ import Home from './Home';
 
 class DoctorList extends Component {
   // let map1 = {this.state.doctors}.map(doctor => doctor);
+constructor(){
+  super();
+  this.state={
+    list: false
+  }
+  this.toggleVisibility = this.toggleVisibility.bind(this);
+}
 
 
-
+toggleVisibility(){
+  document.getElementById('Doctors').className = "hidden";
+}
   render() {
     let doctorCardList = this.props.doctors.map((element, index) =>{
       let info = element.profile;
       return <DoctorCard firstName={info.first_name} key={`id:${index}`} lastName={info.last_name} gender={info.gender} bio ={info.bio}
-        picture={info.image_url}/>
+        picture={info.image_url} onClick={this.toggleVisibility}/>
     });
     console.log(doctorCardList);
     return (
-      <div id="Doctors">
-          // {doctorCardList}
+      <div className="" id="Doctors">
+          {doctorCardList}
       </div>
     );
   }
